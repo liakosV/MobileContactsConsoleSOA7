7c5b502bcbce0be8fac843307c4e4396d44dfca3
LiakosV <billiak@hotmail.com>
2025-01-27 21:04:51 +0200

    Initial Commit

---
+++ src/gr/aueb/cf/mobilecontacts/exceptions/ContactNotFoundException.java
@@ -0,0 +1,7 @@
+package gr.aueb.cf.mobilecontacts.exceptions;
+
+public class ContactNotFoundException extends Exception {
+    public ContactNotFoundException(String message) {
+        super(message);
+    }
+}
